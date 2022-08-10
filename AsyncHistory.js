78271768a271158a37b079aac9bcef91f8024653
dracarys55 <s6263269@gmail.com>
2022-08-10 20:53:59 +0800

    es6

--- AsyncHistory.js
+++ AsyncHistory.js
@@ -4,9 +4,13 @@ AJAX = Asynchronous JavaScript And XML.
 一開始請求的資料格式是使用 XML，後來才改變成我們熟知的JSON格式
 */
 /* 可以用joke API 當範例 */
-/* fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
+const result = fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
   .then((response) => response.json())
-  .then((json) => console.log(json)); */
+  .then((json) => console.log(json));
+setTimeout(() => {
+  // response = result.json();
+  console.log(result);
+}, 3000);
 /* 
 function reqListener() {
   const data = JSON.parse(this.responseText);
@@ -17,11 +21,29 @@ req.onload = reqListener;
 req.open('get', 'https://jsonplaceholder.typicode.com/todos?userId=1', true);
 req.send(); */
 
-let example = new Promise((resolve, reject) => {
-  resolve('resolve');
-  console.log('resolve');
-});
+// let example = new Promise((resolve, reject) => {
+//   resolve('resolve');
+//   console.log('resolve');
+// });
 
-example.then((d) => {
-  console.log(d);
-});
+// example.then((d) => {
+//   console.log(d);
+// });
+
+function get_joke_of_the_day() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Access the result here
+      console.log(this.responseText);
+    }
+  };
+  xhttp.open('GET', 'https://api.jokes.one/jod?category=animal', true);
+  xhttp.setRequestHeader('Content-type', 'application/json');
+  xhttp.setRequestHeader('X-JokesOne-Api-Secret', 'YOUR API HERE');
+  xhttp.send();
+}
+
+get_joke_of_the_day();
+
+console.log(window.fetch);
--- ES6Syntax.js
+++ ES6Syntax.js
@@ -1,1 +1,28 @@
 /* ES6Syntax */
+
+// Template literals  又叫作樣板書寫，你制定好樣板，別人套版即可
+/* using backtick (`) 去解決一般字串遇到的問題
+1.無法多行書寫
+2.要加入變數要一直用 + 號，很煩
+*/
+
+const myName = 'jack';
+const subject = 'ES6';
+const cueerntTime = new Date();
+const hello = `
+hi,
+${myName}
+Do 
+you 
+wanna
+learn 
+${subject}?
+
+${cueerntTime}
+right 
+now?
+`;
+
+console.log(hello);
+
+// Destructuring：解構
